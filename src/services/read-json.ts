caf27239cdeeadfbbbe8bf0ae53ff505332f6802
lowell <lowell.hu@icloud.com>
2025-02-23 18:48:01 +0800

    add test console

--- src/services/read-json.ts
+++ src/services/read-json.ts
@@ -1,5 +1,7 @@
 import { get } from "./base";
 
 export const readJson = async (name: "BEC_2" | "BEC_3"): Promise<string[]> => {
-  return get(`/api/read_dictionary?name=${name}`);
+  const res = await get(`/api/read_dictionary?name=${name}`);
+  console.log({res});
+  return res as string[];
 }
